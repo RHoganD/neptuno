20d612dcfbd10af32ed290db19fb9b94ba5609fc
RHoganD <richpatric@hotmail.com>
2023-06-10 18:37:49 +0000

    Add new features altering the room models

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Room)
+@admin.register(Room)
+class RoomAdmin(SummernoteModelAdmin):
+
+
+    list_display = ('title', 'room_slug', 'price_per_night','featured',)
+    search_fields = ['title', 'featured']
+    list_filter = ('featured', 'is_booked', 'price_per_night', 'capacity', 'room_size',)
+    prepopulated_fields = {'room_slug': ('title',)}
+    summernote_fields = ('content',)
--- hotel/models.py
+++ hotel/models.py
@@ -6,14 +6,19 @@ from django.contrib.auth.models import User
 
 class Room(models.Model):
     title = models.CharField(max_length=30)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name="room_details")
     price_per_night = models.DecimalField(max_digits=8, decimal_places=3)
     room_slug = models.SlugField()
     is_booked = models.BooleanField(default=False)
     capacity = models.IntegerField()
     room_size = models.CharField(max_length=5)
+    featured = models.CharField(
+       max_length=100,
+       choices=[('Standard', 'Standard'), ('Delux', 'Delux'), ('Twin', 'Twin'), ('Premium', 'Premium')]
+   )
     featured_image = CloudinaryField('image', default='placeholder')
-    featured = models.BooleanField(default=False)
+    content = models.TextField()
+    
 
     def __str__(self):
         return self.title
@@ -21,6 +26,6 @@ class Room(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
-
+    
     def __str__(self):
         return self.category_name
