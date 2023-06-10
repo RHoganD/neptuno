3577e70a5fc354ffa15bda623616b9a0d31fca07
RHoganD <richpatric@hotmail.com>
2023-06-10 15:37:10 +0000

    create room and category models

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    title = models.CharField(max_length=30)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=3)
+    room_slug = models.SlugField()
+    is_booked = models.BooleanField(default=False)
+    capacity = models.IntegerField()
+    room_size = models.CharField(max_length=5)
+    featured_image = CloudinaryField('image', default='placeholder')
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
